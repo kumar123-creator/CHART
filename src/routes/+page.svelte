1b477eba6cdd0fc2d65cc729ab2684e3ba987dda
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-10-11 17:20:14 +0530

    Code Updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,66 @@
 <script>
 	import { onMount } from "svelte";
 	import * as Chart from "@syncfusion/ej2-charts";
-  
+	import {DataLabel} from "@syncfusion/ej2-charts";
+
 	let chartData = [];
-  
+
 	const fetchChartData = async () => {
-	  try {
-		const response = await fetch(
-		  "https://api.recruitly.io/api/dashboard/sales/data/opportunitymonthlyusermetrics?start=01%2F10%2F2022&end=01%2F10%2F2023&apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77"
-		);
-		const data = await response.json();
-		chartData = data;
-		createChart();
-	  } catch (error) {
-		console.error("Error fetching chart data", error);
-	  }
+		try {
+			const response = await fetch(
+				"https://api.recruitly.io/api/dashboard/sales/data/opportunitymonthlyusermetrics?start=01%2F10%2F2022&end=01%2F10%2F2023&apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77"
+			);
+			const data = await response.json();
+			 chartData = data; // Make chartData reactive
+			 console.log(chartData);
+			createChart();
+		} catch (error) {
+			console.error("Error fetching chart data", error);
+		}
 	};
-  
+
 	const createChart = () => {
-	  const chartElement = document.getElementById("chart");
-	  if (chartElement) {
-		new Chart.Chart({
-		  // Chart configuration
-		  series: [
-			{
-			  dataSource: chartData,
-			  xName: "monthLabel",
-			  yName: "Andy Barnes", // Adjust for other series
-			  name: "Andy Barnes",
-			  type: "Line",
-			},
-			// Add more series for other users if needed
-		  ],
-		  primaryXAxis: {
-			title: "Month",
-		  },
-		  primaryYAxis: {
-			title: "Oppurtunity Value",
-		  },
-		}).appendTo(chartElement);
-	  }
+		const chartElement = document.getElementById("chart");
+		if (chartElement) {
+			new Chart.Chart({
+				// Chart configuration
+				series: [
+					{
+						dataSource: chartData,
+						xName: "monthLabel",
+						yName: "0", // Adjust for the actual field name in your data
+						name: "Bob Shaw",
+						type: "Line",
+					},
+					{
+						dataSource: chartData,
+						xName: "monthLabel",
+						yName: "10", // Adjust for the actual field name in your data
+						name: "Andy Barnes",
+						type: "Line",
+					},
+					{
+						dataSource: chartData,
+						xName: "monthLabel",
+						yName: "0", // Adjust for the actual field name in your data
+						name: "Gary Williams",
+						type: "Line",
+					},
+				],
+				primaryXAxis: {
+					title: "Month",
+				},
+				primaryYAxis: {
+					title: "Opportunity Value",
+				},
+			}).appendTo(chartElement);
+		}
 	};
-  
+
 	onMount(() => {
-	  fetchChartData();
+		fetchChartData();
 	});
-  </script>
+</script>
   
   <style>
 	  #chart {
